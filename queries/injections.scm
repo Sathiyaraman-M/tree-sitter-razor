3b5c5f6db957368fa7385475353779ae13933c64
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2026-08-20 12:20:37 +0530

    Add injection for C# code in razor_explicit_expression

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,3 +1,7 @@
 ((code_block
   body: (csharp_code) @injection.content)
  (#set! injection.language "c_sharp"))
+
+((razor_explicit_expression
+  body: (csharp_expression) @injection.content)
+ (#set! injection.language "c_sharp"))
